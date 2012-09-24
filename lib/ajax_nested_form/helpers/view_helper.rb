e2a040fe399d65671b0e7e5acf5d7ab5bf2c8b10
sergio1990 <sergeg1990@gmail.com>
2012-09-24 09:56:27 +0300

    -Added html options param to helper methods

--- lib/ajax_nested_form/helpers/view_helper.rb
+++ lib/ajax_nested_form/helpers/view_helper.rb
@@ -2,16 +2,16 @@ module AjaxNestedForm
 	module Helpers
 		module ViewHelper
 
-			def link_to_remove_fields(name, f, container)
-			  f.hidden_field(:_destroy) + link_to_function(name, "AjaxNestedForm.removeFields(this, \"#{container}\")".html_safe)
+			def link_to_remove_fields(name, f, container, html_options = {})
+			  f.hidden_field(:_destroy) + link_to_function(name, "AjaxNestedForm.removeFields(this, \"#{container}\")".html_safe, html_options)
 			end
 
-			def link_to_add_fields(name, f, association, container, position = "last")
+			def link_to_add_fields(name, f, association, container, position = "last", html_options = {})
 				new_object = f.object.class.reflect_on_association(association).klass.new
 				fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
 				  render(association.to_s.singularize + "_fields", :f => builder)
 				end
-				link_to_function(name, "AjaxNestedForm.addFields(this, \"#{association}\", \"#{escape_javascript(fields)}\", \"#{container}\", \"#{position}\")".html_safe)
+				link_to_function(name, "AjaxNestedForm.addFields(this, \"#{association}\", \"#{escape_javascript(fields)}\", \"#{container}\", \"#{position}\")".html_safe, html_options)
 			end
 
 		end
